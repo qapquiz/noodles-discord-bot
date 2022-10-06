384c0db6e7fc48ef174fd22889f5975663b8dfcf
ariya-l <ariya.l@dv.co.th>
2022-10-06 23:02:20 +0700

    feat: add reactionAdd event

--- mod.ts
+++ mod.ts
@@ -1,19 +1,25 @@
-import {
-	createBot,
-	Intents,
-	startBot,
-} from "./deps.ts";
-import 'https://deno.land/x/dotenv@v3.2.0/load.ts';
+import { createBot, EmojiToggle, Intents, startBot } from "./deps.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+const verifyChannel = BigInt("1021807055503364187");
 
 const bot = createBot({
 	token: Deno.env.get("DISCORD_TOKEN")!,
 	intents: Intents.Guilds | Intents.GuildMessages,
-    botId: BigInt(Deno.env.get("CLIENT_ID")!),
+	botId: BigInt(Deno.env.get("CLIENT_ID")!),
 	events: {
 		ready() {
 			console.log("Successfully connected to gateway");
 		},
 	},
 });
+
+bot.events.reactionAdd = function (bot, payload) {
+	const { userId, channelId, messageId, emoji } = payload;
+
+	if (channelId === verifyChannel) return
+
+	console.log("payload:", JSON.stringify(payload, null, 2));
+};
 
 await startBot(bot);
