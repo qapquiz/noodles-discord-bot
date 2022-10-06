4e7f8c5a2c0980fdbc90e471df1e254fca30a08d
ariya-l <ariya.l@dv.co.th>
2022-10-06 20:15:40 +0700

    feat: change the way to load env and add BOT_ID

--- deps.ts
+++ deps.ts
@@ -1,2 +1,1 @@
 export * from "https://deno.land/x/discordeno@16.0.1/mod.ts";
-export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,15 +1,14 @@
 import {
-    dotEnvConfig,
 	createBot,
 	Intents,
 	startBot,
 } from "./deps.ts";
-
-dotEnvConfig({ export: true });
+import 'https://deno.land/x/dotenv@v3.2.0/load.ts';
 
 const bot = createBot({
 	token: Deno.env.get("DISCORD_TOKEN")!,
 	intents: Intents.Guilds | Intents.GuildMessages,
+    botId: BigInt(Deno.env.get("CLIENT_ID")!),
 	events: {
 		ready() {
 			console.log("Successfully connected to gateway");
