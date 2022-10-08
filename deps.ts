a1afebae7f471c736ddcacf2d4ac802ed4eeb258
ariya-l <ariya.l@dv.co.th>
2022-10-09 00:11:36 +0700

    feat: when react with checkmark bot will assign verified role to that user

--- deps.ts
+++ deps.ts
@@ -1,1 +1,8 @@
 export * from "https://deno.land/x/discordeno@16.0.1/mod.ts";
+export {
+	enableCachePlugin,
+	enableHelpersPlugin,
+	enableCacheSweepers,
+	enablePermissionsPlugin,
+	type BotWithCache,
+} from "https://deno.land/x/discordeno@16.0.1/plugins/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,28 +1,66 @@
-import { createBot, Intents, startBot } from "./deps.ts";
+import {
+	createBot,
+	enableCachePlugin,
+	enableHelpersPlugin,
+	enableCacheSweepers,
+	enablePermissionsPlugin,
+	Intents,
+	startBot,
+	BotWithCache,
+	Message,
+	Bot,
+} from "./deps.ts";
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
-const verifyChannel = BigInt("1021807055503364187");
+const ChannelId = {
+	Verify: 1021807055503364187n,
+};
 
 const bot = createBot({
 	token: Deno.env.get("DISCORD_TOKEN")!,
-	intents: Intents.Guilds | Intents.GuildMessages,
+	intents:
+		Intents.Guilds |
+		Intents.GuildMessages |
+		Intents.MessageContent |
+		Intents.GuildMessageReactions,
 	botId: BigInt(Deno.env.get("CLIENT_ID")!),
 	events: {
 		ready() {
 			console.log("Successfully connected to gateway");
 		},
+		messageCreate(bot: Bot, message: Message) {
+			console.log("message:", message);
+			if (message.isFromBot) {
+				return;
+			}
+
+			bot.helpers.sendMessage(message.channelId, {
+				content: "verify by react to ✅ below here",
+			});
+		},
+		async reactionAdd(bot: Bot, { userId, channelId, emoji, guildId }) {
+			if (guildId === undefined) {
+				return;
+			}
+
+			if (
+				channelId !== ChannelId.Verify &&
+				emoji.name !== "✅"
+			) {
+				return;
+			}
+
+			const verifiedRole = (await bot.helpers.getRoles(guildId)).find(
+				(role) => role.name === "verified"
+			);
+			verifiedRole && bot.helpers.addRole(guildId, userId, verifiedRole.id);
+		},
 	},
 });
 
-bot.events.reactionAdd = function (bot, payload) {
-	console.log("reactionAdd");
-	const { userId, channelId, messageId, emoji } = payload;
-
-	if (channelId !== verifyChannel) {
-		return;
-	}
-
-	console.log("payload:", JSON.stringify(payload, null, 2));
-};
+enableHelpersPlugin(bot);
+enableCachePlugin(bot);
+enableCacheSweepers(bot as BotWithCache);
+enablePermissionsPlugin(bot as BotWithCache);
 
 await startBot(bot);
