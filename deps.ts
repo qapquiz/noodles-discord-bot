d0b02d8b1b999f0771933e948e87e7ee3afa3cce
qapquiz <arm.armariya@gmail.com>
2023-01-03 21:37:02 +0700

    deps: update version server

--- deps.ts
+++ deps.ts
@@ -6,4 +6,4 @@ export {
 	enablePermissionsPlugin,
 	type BotWithCache,
 } from "https://deno.land/x/discordeno@17.1.0/plugins/mod.ts";
-export { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.170.0/http/server.ts";
