747fb2cd7739e33fd1134caa9b459eed25830d62
qapquiz <arm.armariya@gmail.com>
2023-01-16 22:36:06 +0700

    feat: add auto tweet to twitter-feed channel

--- mod.ts
+++ mod.ts
@@ -19,6 +19,7 @@ import {
 	reactionRemove,
 	ready,
 } from "./src/events/mod.ts";
+import { ServerConstants } from "./src/constant.ts";
 
 const bot = createBot({
 	token: Deno.env.get("DISCORD_TOKEN")!,
@@ -57,9 +58,17 @@ await bot.helpers.editBotStatus({
 
 const TWITTER_FEED_HOOK_ROUTE = new URLPattern({ pathname: "/twitter-feed" });
 
-const handler = (req: Request): Response => {
-	if (TWITTER_FEED_HOOK_ROUTE.exec(req.url)) {
-		console.log("twitter feed req:", req);
+const handler = async (req: Request): Promise<Response> => {
+	const serverConfig = ServerConstants[0];
+
+	if (TWITTER_FEED_HOOK_ROUTE.exec(req.url) && req.body) {
+		const body = await req.text();
+		await bot.helpers.sendMessage(
+			serverConfig.Channel.twitterFeed,
+			{
+				content: `Ayo, **NoodlesDudeNFT** just posted a new Tweet!\n${body}`
+			},
+		);
 	}
 
 	return new Response("still alive!");
