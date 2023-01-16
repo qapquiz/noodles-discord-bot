05a3b79f9d040f30a7e325be7c8e5af92629c562
qapquiz <arm.armariya@gmail.com>
2023-01-17 00:50:56 +0700

    feat: update version

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export * from "https://deno.land/x/discordeno@17.1.0/mod.ts";
+export * from "https://deno.land/x/discordeno@18.0.0/mod.ts";
 export {
 	enableCachePlugin,
 	enableHelpersPlugin,
 	enableCacheSweepers,
 	enablePermissionsPlugin,
 	type BotWithCache,
-} from "https://deno.land/x/discordeno@17.1.0/plugins/mod.ts";
-export { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+} from "https://deno.land/x/discordeno@18.0.0/plugins/mod.ts";
+export { serve } from "https://deno.land/std@0.172.0/http/server.ts";
